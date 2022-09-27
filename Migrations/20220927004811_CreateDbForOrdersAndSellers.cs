76b2f250f348be180273d1926609d887f54c0e15
harry yazawa <harryazawa@gmail.com>
2022-09-26 22:28:59 -0300

    doc: adding Migrations

---
+++ Migrations/20220927004811_CreateDbForOrdersAndSellers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tech_test_payment_api.Migrations
+{
+    public partial class CreateDbForOrdersAndSellers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderSellers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderSellers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrdersTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SellerId = table.Column<int>(type: "int", nullable: false),
+                    Items = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AvailableStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrderStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastUpdate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdersTable", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderSellers");
+
+            migrationBuilder.DropTable(
+                name: "OrdersTable");
+        }
+    }
+}
